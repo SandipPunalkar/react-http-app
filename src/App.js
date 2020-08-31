b51eb967dfd965ec3ad38bd8accde85f4730b44e
Sandip Punalkar <sandippunalkar9@gmail.com>
2020-08-31 18:47:54 +0530

    Optimistic

--- src/App.js
+++ src/App.js
@@ -31,11 +31,19 @@ class Appp extends Component {
     posts[index] = { ...post };
     this.setState({ posts });
   };
-
   handleDelete = async (post) => {
-    await axios.delete(apiEndpoint + "/" + post.id);
+    const originalPosts = this.state.posts;
+
     const posts = this.state.posts.filter((p) => p.id !== post.id);
     this.setState({ posts });
+
+    try {
+      await axios.delete(apiEndpoint + "/" + post.id);
+      throw new Error("");
+    } catch (ex) {
+      alert("Something failed");
+      this.setState({ posts: originalPosts });
+    }
   };
 
   render() {
